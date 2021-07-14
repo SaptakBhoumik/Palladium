3be27214156f377e4c7d06079b77f7317706e87d
Saptak Bhoumik <saptakbhoumik@gmail.com>
2021-07-14 21:30:07 +0530

    Add files via upload

---
+++ Model_Training/training_lung_disease_detection_model.py
@@ -0,0 +1,179 @@
+#Load libraries
+import os
+import numpy as np
+import torch
+import glob
+import torch.nn as nn
+from torchvision.transforms import transforms
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from torch.autograd import Variable
+import torchvision
+import pathlib
+
+#checking for device
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+#Transforms
+transformer=transforms.Compose([
+    transforms.Resize((150,150)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),  #0-255 to 0-1, numpy to tensors
+    transforms.Normalize([0.5,0.5,0.5], # 0-1 to [-1,1] , formula (x-mean)/std
+                        [0.5,0.5,0.5])
+])
+
+#Dataloader
+
+#Path for training and testing directory
+train_path='./dataset/train'
+test_path='./dataset/test'
+print("Loading data")
+train_loader=DataLoader(
+    torchvision.datasets.ImageFolder(train_path,transform=transformer),
+    batch_size=64, shuffle=True
+)
+test_loader=DataLoader(
+    torchvision.datasets.ImageFolder(test_path,transform=transformer),
+    batch_size=32, shuffle=True
+)
+print("Done")
+#categories
+root=pathlib.Path(train_path)
+classes=sorted([j.name.split('/')[-1] for j in root.iterdir()])
+print(f"Categories {classes}")
+
+#CNN Network
+
+
+class ConvNet(nn.Module):
+    def __init__(self,num_classes=len(classes)):
+        super(ConvNet,self).__init__()
+        
+        #Output size after convolution filter
+        #((w-f+2P)/s) +1
+        
+        #Input shape= (256,3,150,150)
+        
+        self.conv1=nn.Conv2d(in_channels=3,out_channels=12,kernel_size=3,stride=1,padding=1)
+        #Shape= (256,12,150,150)
+        self.bn1=nn.BatchNorm2d(num_features=12)
+        #Shape= (256,12,150,150)
+        self.relu1=nn.ReLU()
+        #Shape= (256,12,150,150)
+        
+        self.pool=nn.MaxPool2d(kernel_size=2)
+        #Reduce the image size be factor 2
+        #Shape= (256,12,75,75)
+        
+        
+        self.conv2=nn.Conv2d(in_channels=12,out_channels=20,kernel_size=3,stride=1,padding=1)
+        #Shape= (256,20,75,75)
+        self.relu2=nn.ReLU()
+        #Shape= (256,20,75,75)
+        
+        
+        
+        self.conv3=nn.Conv2d(in_channels=20,out_channels=32,kernel_size=3,stride=1,padding=1)
+        #Shape= (256,32,75,75)
+        self.bn3=nn.BatchNorm2d(num_features=32)
+        #Shape= (256,32,75,75)
+        self.relu3=nn.ReLU()
+        #Shape= (256,32,75,75)
+        
+        
+        self.fc=nn.Linear(in_features=75 * 75 * 32,out_features=num_classes)
+        
+        
+        
+        #Feed forwad function
+        
+    def forward(self,input):
+        output=self.conv1(input)
+        output=self.bn1(output)
+        output=self.relu1(output)
+            
+        output=self.pool(output)
+            
+        output=self.conv2(output)
+        output=self.relu2(output)
+            
+        output=self.conv3(output)
+        output=self.bn3(output)
+        output=self.relu3(output)
+            
+            
+            #Above output will be in matrix form, with shape (256,32,75,75)
+            
+        output=output.view(-1,32*75*75)
+            
+            
+        output=self.fc(output)
+            
+        return output
+
+model=ConvNet(num_classes=len(classes)).to(device)
+#Optmizer and loss function
+optimizer=Adam(model.parameters(),lr=0.001,weight_decay=0.0001)
+loss_function=nn.CrossEntropyLoss()
+num_epochs=int(input("How many epochs you want(15 prefered) "))
+#calculating the size of training and testing images
+train_count=len(glob.glob(train_path+'/**/*.jpg'))
+test_count=len(glob.glob(test_path+'/**/*.jpg'))
+print(f"Train count={train_count},Test count={test_count}")
+
+#Model training and saving best model
+
+best_accuracy=0.0
+
+for epoch in range(num_epochs):
+    
+    #Evaluation and training on training dataset
+    model.train()
+    train_accuracy=0.0
+    train_loss=0.0
+    
+    for i, (images,labels) in enumerate(train_loader):
+        if torch.cuda.is_available():
+            images=Variable(images.cuda())
+            labels=Variable(labels.cuda())
+            
+        optimizer.zero_grad()
+        
+        outputs=model(images)
+        loss=loss_function(outputs,labels)
+        loss.backward()
+        optimizer.step()
+        
+        
+        train_loss+= loss.cpu().data*images.size(0)
+        _,prediction=torch.max(outputs.data,1)
+        
+        train_accuracy+=int(torch.sum(prediction==labels.data))
+        
+    train_accuracy=train_accuracy/train_count
+    train_loss=train_loss/train_count
+    
+    
+    # Evaluation on testing dataset
+    model.eval()
+    
+    test_accuracy=0.0
+    for i, (images,labels) in enumerate(test_loader):
+        if torch.cuda.is_available():
+            images=Variable(images.cuda())
+            labels=Variable(labels.cuda())
+            
+        outputs=model(images)
+        _,prediction=torch.max(outputs.data,1)
+        test_accuracy+=int(torch.sum(prediction==labels.data))
+    
+    test_accuracy=test_accuracy/test_count
+    
+    
+    print('Epoch: '+str(epoch)+' Train Loss: '+str(train_loss)+' Train Accuracy: '+str(train_accuracy)+' Test Accuracy: '+str(test_accuracy))
+    
+    #Save the best model
+    if test_accuracy>best_accuracy:
+        torch.save(model.state_dict(),'lung_disease_detection_model.pth')
+        best_accuracy=test_accuracy
---
+++ palladium/Palladium.py
@@ -0,0 +1,107 @@
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
+import concurrent.futures
+import threading
+import os
+import cnn
+
+start_page = r'''<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+            body{overflow: hidden; color:#4cabff;}
+            a:link, a:visited {
+                background-color: #4cabff;
+                color: white;
+                padding: 14px 25px;
+                text-align: center;
+                border-radius:30px;
+                text-decoration: none;
+                display: inline-block;
+            }
+            
+            a:hover, a:active {
+                background-color: blue;
+            }</style></head>
+            <body>
+                <div align="center" style="font-size:30px;font-family:Arial"><b>Upload the x-ray image of your chest</b></div><br><a href="#" download><div align="center" style="font-size:20px;font-family:Arial">Click here to choose a file</div></a>
+            </body>
+            </html>'''
+
+
+
+result_page=r'''<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+            body{overflow: hidden; color:#4cabff;}
+            a:link, a:visited {
+                background-color: #4cabff;
+                color: white;
+                padding: 14px 25px;
+                text-align: center;
+                border-radius:30px;
+                text-decoration: none;
+                display: inline-block;
+            }
+            
+            a:hover, a:active {
+                background-color: blue;
+            }</style></head>
+            <body>
+                <div align="center" style="font-size:30px;font-family:Arial"><b>Upload the x-ray image of your chest</b></div><br><a href="#" download><div align="center" style="font-size:20px;font-family:Arial">Click here to choose a file</div></a><br>
+           <br><div align="center" style="font-size:20px;font-family:Arial"><b>report_and_i_will_replace_it</b></div><br><div align="center" style="font-size:20px;font-family:Arial"><b>symptoms_and_i_will_replace_it</b></div>
+            </body></html>'''
+
+
+
+class Widget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(Widget, self).__init__(parent)
+        self.setWindowTitle("PALLADIUM")
+        self.setFixedSize(330, 620)
+        self.setStyleSheet('background-color: white;color:white')
+        self.view = QtWebEngineWidgets.QWebEngineView()
+        self.view .setContextMenuPolicy(QtCore.Qt.NoContextMenu)
+        self.view.page().profile().downloadRequested.connect(self.on_downloadRequested)
+        self.view.setHtml(start_page)
+        hbox = QtWidgets.QHBoxLayout(self)
+        hbox.addWidget(self.view)
+        self.setWindowIcon(QtGui.QIcon(os.path.join('images', 'source.jpeg')))
+
+
+#yes you are right i am using download requested to call python function
+    @QtCore.pyqtSlot("QWebEngineDownloadItem*")
+    def on_downloadRequested(self):
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open file", "", "Image(*.*);;")
+        print(filename)
+        try:
+            report=cnn.prediction(filename)
+            if report.lower()=="normal":
+                report="Congratulations your lungs are in normal condition"
+                symptoms=""
+            if report.lower()=="covid19":
+                report="Looks like you are infected by covid 19. You should go and see a doctor"
+                symptoms="Some of the symptoms of covid19 are fever, dry cough, tiredness, loss of taste or smell,difficulty breathing or shortness of breath and headache"
+            if report.lower()=="pneumonia":
+                report="Looks like you are infected by pneumonia. You should go and see a doctor"
+                symptoms="Some of the symptoms of pneumonia are cough, which may produce greenish, yellow or even bloody mucus, fever, shortness of breath rapid, shallow breathing, sharp or stabbing chest pain that gets worse when you breathe deeply or cough, loss of appetite, low energy, and fatigue."
+        except:
+            report="Unable to process image"
+            symptoms=""
+        result=result_page.replace("report_and_i_will_replace_it",report)
+        result=result.replace("symptoms_and_i_will_replace_it",symptoms)
+        self.view.setHtml(result)
+
+        
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    w = Widget()
+    w.show()
+    sys.exit(app.exec_())
