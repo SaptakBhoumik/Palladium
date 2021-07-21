01865652d1cf8ff141c7eb7cb15f7367d2993092
Saptak Bhoumik <saptakbhoumik@gmail.com>
2021-07-21 20:48:57 +0530

    Add files via upload

---
+++ examples/ct_example.py
@@ -0,0 +1,127 @@
+import torch
+import torch.nn as nn
+from torchvision.transforms import transforms
+from torch.autograd import Variable
+import torch.functional as F
+from io import open
+from PIL import Image
+import glob
+
+
+#categories
+classes=['COVID19', 'NORMAL']
+
+#CNN Network
+def pil_loader(path):
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+class ConvNet(nn.Module):
+    def __init__(self,num_classes=2):
+        super(ConvNet,self).__init__()
+        
+        #Output size after convolution filter
+        #((w-f+2P)/s) +1
+        
+        #Input shape= (256,3,150,150)
+        
+        self.conv1=nn.Conv2d(in_channels=3,out_channels=12,kernel_size=3,stride=1,padding=1)
+        #Shape= (256,12,150,150)
+        self.bn1=nn.BatchNorm2d(num_features=12)
+        #Shape= (256,12,150,150)
+        self.relu1=nn.ReLU()
+        #Shape= (256,12,150,150)
+        
+        self.pool=nn.MaxPool2d(kernel_size=2)
+        #Reduce the image size be factor 2
+        #Shape= (256,12,75,75)
+        
+        
+        self.conv2=nn.Conv2d(in_channels=12,out_channels=20,kernel_size=3,stride=1,padding=1)
+        #Shape= (256,20,75,75)
+        self.relu2=nn.ReLU()
+        #Shape= (256,20,75,75)
+        
+        
+        
+        self.conv3=nn.Conv2d(in_channels=20,out_channels=32,kernel_size=3,stride=1,padding=1)
+        #Shape= (256,32,75,75)
+        self.bn3=nn.BatchNorm2d(num_features=32)
+        #Shape= (256,32,75,75)
+        self.relu3=nn.ReLU()
+        #Shape= (256,32,75,75)
+        
+        
+        self.fc=nn.Linear(in_features=75 * 75 * 32,out_features=num_classes)
+        
+        
+        
+        #Feed forwad function
+        
+    def forward(self,input):
+        output=self.conv1(input)
+        output=self.bn1(output)
+        output=self.relu1(output)
+            
+        output=self.pool(output)
+            
+        output=self.conv2(output)
+        output=self.relu2(output)
+            
+        output=self.conv3(output)
+        output=self.bn3(output)
+        output=self.relu3(output)
+            
+            
+            #Above output will be in matrix form, with shape (256,32,75,75)
+            
+        output=output.view(-1,32*75*75)
+            
+            
+        output=self.fc(output)
+            
+        return output
+
+
+checkpoint=torch.load('ctscan_lung_disease_detection_model.pth')
+model=ConvNet(num_classes=len(classes))
+model.load_state_dict(checkpoint)
+model.eval()
+#Transforms
+transformer=transforms.Compose([
+    transforms.Resize((150,150)),
+    transforms.ToTensor(),  #0-255 to 0-1, numpy to tensors
+    transforms.Normalize([0.5,0.5,0.5], # 0-1 to [-1,1] , formula (x-mean)/std
+                        [0.5,0.5,0.5])
+])
+#prediction function
+
+
+#prediction function
+def prediction(img_path):
+    
+    image=pil_loader(img_path)
+    
+    image_tensor=transformer(image).float()
+    
+    
+    image_tensor=image_tensor.unsqueeze_(0)
+    
+    if torch.cuda.is_available():
+        image_tensor.cuda()
+        
+    input=Variable(image_tensor)
+    
+    
+    output=model(input)
+    
+    index=output.data.numpy().argmax()
+    
+    pred=classes[index]
+    
+    return pred
+
+
+a=input("Enter the path to the image you want to predict ")
+print(prediction(a))
